4ccb06e0f1f077dc322082ac94437d82672c0d58
Vikas M <vikasmnmohan@gmail.com>
2026-03-17 00:27:53 +0530

    Fix SplitText TypeScript errors

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -12,15 +12,18 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
+
   if (window.innerWidth < 900) return;
-  const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
-  const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
+
+  const paras = document.querySelectorAll<ParaElement>(".para");
+  const titles = document.querySelectorAll<ParaElement>(".title");
 
   const TriggerStart = window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
   const ToggleAction = "play pause resume reverse";
 
-  paras.forEach((para: ParaElement) => {
+  paras.forEach((para) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -32,12 +35,12 @@ export default function setSplitText() {
     });
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para.split?.words || [],
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
         scrollTrigger: {
-          trigger: para.parentElement?.parentElement,
+          trigger: para.parentElement?.parentElement || para,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
@@ -48,22 +51,25 @@ export default function setSplitText() {
       }
     );
   });
-  titles.forEach((title: ParaElement) => {
+
+  titles.forEach((title) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title.split?.chars || [],
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
         scrollTrigger: {
-          trigger: title.parentElement?.parentElement,
+          trigger: title.parentElement?.parentElement || title,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
